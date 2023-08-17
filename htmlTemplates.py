264a8bbee69ac632258b33150e661e75787dd069
VIVEK SINHA <vivek.sinha@globtier.co.in>
2023-08-17 08:02:07 +0530

    initial commit

---
+++ htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://chatbot-project.s3.ap-south-1.amazonaws.com/botgo/avatars/avatar1.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://img.freepik.com/premium-vector/people-happy-face-man-icon_24640-19215.jpg">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
